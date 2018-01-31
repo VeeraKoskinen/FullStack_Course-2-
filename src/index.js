a5712be2130f6c4a4e986d7453377e34ae6cdd01
Veera Koskinen <veera.koskinen@ren.to>
2018-01-31 15:12:36 +0200

    Tehtävä 2.14

--- src/index.js
+++ src/index.js
@@ -18,23 +18,18 @@ class App extends React.Component {
     }
     
     makeFilteredList = () => {
-        console.log("henkilölista ennen:")
-        console.log(this.state.persons)
-        console.log("state:")
-        console.log(this.state)
         return this.state.persons.filter(person => this.checkPerson(person))  
-        console.log("henkilölistaus jälkeen:")
-        console.log(this.state.persons)  
     }
 
     checkPerson = (person) => {
         let inStudy = person.name.toLowerCase()
         if (inStudy.includes(this.state.filter.toLowerCase())) {
             console.log("true")
-            return true }
-        else {
+            return true 
+        } else {
             console.log("false")
-            return false }
+            return false 
+        }
     }
 
     /* Tehty henkilön ja numeron perusteella  */
@@ -42,20 +37,27 @@ class App extends React.Component {
         let arvo = false
         this.state.persons.forEach((person) => { 
             if (person.name === newPerson.name || person.number === newPerson.number) {
-                arvo = true
-        
+                arvo = true        
             } 
         })    
         return arvo      
     } 
 
     addNewPerson = (newPerson) => {
+
         if (this.isPersonOnTheList(newPerson)) {
             alert('Tämä henkilö löytyy jo listalta!')  
         } else {
-            let newList = this.state.persons.slice()
-            newList.push(newPerson)
-            this.setState({persons: newList})
+
+            const personObject = {
+                name : newPerson.name,
+                number : newPerson.number
+            }
+
+            axios.post('http://localhost:3001/persons', personObject)
+            .then(response => {
+                this.setState({persons: this.state.persons.concat(response.data)})
+            })
         }            
         
     }
@@ -78,29 +80,28 @@ class App extends React.Component {
         console.log('will mount')
         axios
             .get('http://localhost:3001/persons')
-            .then(response => { 
-                const db = response.data
-                console.log(db)
+            .then(response => {  
                 console.log("promise fullfilled")
                 this.setState({persons: response.data})
                 console.log(this.state.persons)
         })
     }
 
-  render() {
-    return (
-      <div>
-        <h2>Puhelinluettelo</h2>
-        <Input name={"rajaa näytettäviä"} value={this.state.filter} handler={this.handleFilterChange} />
-        <h3>Lisää uusi</h3>
-        <AddingForm function={this.addNewPerson}/>
-        <h3>Numerot</h3>
-        <div>
-            {this.personListing()}
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>Puhelinluettelo</h2>
+                <Input name={"rajaa näytettäviä"} value={this.state.filter} handler={this.handleFilterChange} />
+                <h3>Lisää uusi</h3>
+                <AddingForm function={this.addNewPerson}/>
+                <h3>Numerot</h3>
+
+                <div>
+                    {this.personListing()}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App
